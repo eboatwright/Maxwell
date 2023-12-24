bc1c50706f57ac4ee629129181df1a051319fb34
eboatwright <64672325+eboatwright@users.noreply.github.com>
2023-12-24 11:17:13 -0600

    Finished Caro Kann opening lines

--- src/opening_book.rs
+++ src/opening_book.rs
@@ -16,11 +16,11 @@ impl OpeningBook {
 
 				"e2e4 e7e5 g1f3 b8c6 d2d4 e5d4 f3d4 g8f6 d4c6 b7c6 e4e5 d8e7 d1e2 f6d5",
 
-				// Caro Kann TODO
+				// Caro Kann
 				"e2e4 c7c6 d2d4 d7d5 b1c3 d5e4 c3e4 g8f6 e4f6 e7f6 g1f3 f8d6 f1e2 e8g8 e1g1 f8e8 c2c4",
-				"e2e4 c7c6 d2d4 d7d5 e4d5",
-				"e2e4 c7c6 d2d4 d7d5 e4e5",
-				"e2e4 c7c6 g1f3 d7d5",
+				"e2e4 c7c6 d2d4 d7d5 e4d5 c6d5 c2c4 g8f6 b1c3 b8c6 b1f3 e7e6 c4c5 f8e7 f1d3 e8g8 a2a3 b7b6 b2b4 b6c5 b4c5 e6e5",
+				"e2e4 c7c6 d2d4 d7d5 e4e5 c8f5 g1f3 e7e6 f1e2 g8e7 e1g1 c6c5 d4c5 b8d7 c1e3 e7c6",
+				"e2e4 c7c6 g1f3 d7d5 b1c3 c8g4 f1e2 g8f6 h2h3 g4f3 e2f3 d5e4 c3e4 e7e6 d2d4",
 
 				// Ruy Lopez
 				"e2e4 e7e5 g1f3 b8c6 f1b5 a7a6 b5a4 g8f6 e1g1 f8c5",
@@ -39,9 +39,9 @@ impl OpeningBook {
 				"d2d4 g8f6 c2c4 e7e6 b1c3 f8b4 e2e3 e8g8 f1d3 d7d5 g1e2 d5c4 d3c4 e6e5 e1g1 b8c6",
 				"d2d4 g8f6 c2c4 d7d5 g1f3 e7e6 b1c3 f8e7 c1f4 e8g8 e2e3 b7b6 f1d3 d5c4 d3c4 c8b7 e1g1",
 
-				"Xc2c4 e7e5 b1c3 g8f6",
-
 				"e2e4 c7c5 g1f3 d7d6 d2d4 c5d4 f3d4 g8f6 b1c3 b8c6 f1c4 e7e6 c1e3 c8d7 d1e2 a7a6",
+
+				"Xc2c4 e7e5 b1c3 g8f6",
 				"e2e4 c7c5 Xb1c3 b8c6 f1b5 c6d4 b5c4 a7a6 g1f3 b7b5 c4e2 c8b7 e1g1 e7e6 d2d3 d4e2 d1e2",
 			],
 		}
