9df46ab6ad847d644bd6735f8a0c2d60aceda2eb
eboatwright <64672325+eboatwright@users.noreply.github.com>
2024-02-02 15:51:07 -0600

    Moved TT to a Vec

--- src/transposition_table.rs
+++ src/transposition_table.rs
@@ -9,7 +9,7 @@ use crate::move_data::MoveData;
 use std::mem::size_of;
 
 pub const MEGABYTE: usize = 1024 * 1024;
-pub const ENTRY_SIZE: usize = size_of::<u32>() + size_of::<TranspositionData>();
+pub const ENTRY_SIZE: usize = size_of::<Option<TranspositionData>>();
 
 #[derive(Copy, Clone, PartialEq, Debug)]
 pub enum EvalBound {
@@ -30,8 +30,10 @@ pub struct TranspositionData {
 
 pub struct TranspositionTable {
 	size_in_mb: usize,
+	entries: usize,
 	length: usize,
-	pub table: HashMap<u32, TranspositionData>,
+	// Still not sure if I should use a Vec<Option> or HashMap, but I've been tipped to use Vec so I'll do that :>
+	pub table: Vec<Option<TranspositionData>>,
 
 	pub hits: u128,
 }
@@ -42,14 +44,15 @@ impl TranspositionTable {
 
 		Self {
 			size_in_mb,
+			entries: 0,
 			length,
-			table: HashMap::with_capacity(length),
+			table: vec![None; length],
 
 			hits: 0,
 		}
 	}
 
-	pub fn get_index(&self, key: u64) -> u32 { (key % (self.length as u64)) as u32 }
+	pub fn get_index(&self, key: u64) -> usize { (key as usize) % self.length }
 
 	pub fn store(&mut self, key: u64, depth: u8, ply: u8, evaluation: i32, best_move: MoveData, eval_bound: EvalBound) {
 		if self.length == 0 {
@@ -64,24 +67,25 @@ impl TranspositionTable {
 
 		let index = self.get_index(key);
 
-		if let Some(data) = self.table.get(&index) {
-			// If we already have a deeper depth, then we don't care about the new
+		if let Some(data) = self.table[index] {
+			// If we already have a deeper depth, then we don't care about the shallower
 			// data so just return
 			if data.depth > depth {
 				return;
 			}
 
-			// If the new data and the old data were at the exact same depth, prefer
-			// EvalBound::Exact over Lower or Upper bounds
+			// If the new data and the old data were at the exact same depth, prefer an exact evaluation
 			if data.depth == depth
 			&& (data.eval_bound == EvalBound::Exact
 			|| eval_bound != EvalBound::Exact) {
 				return;
 			}
+		} else {
+			self.entries += 1;
 		}
 
-		self.table.insert(index,
-			TranspositionData {
+		self.table[index] =
+			Some(TranspositionData {
 				key,
 				depth,
 				evaluation: fixed_mate_evaluation,
@@ -92,7 +96,7 @@ impl TranspositionTable {
 	}
 
 	pub fn lookup(&mut self, key: u64, ply: u8, depth: u8, alpha: i32, beta: i32) -> (Option<i32>, Option<MoveData>) {
-		if let Some(data) = self.table.get(&self.get_index(key)) {
+		if let Some(data) = self.table[self.get_index(key)] {
 			if data.key == key {
 				self.hits += 1;
 				// data.age = 0;
@@ -121,7 +125,7 @@ impl TranspositionTable {
 	}
 
 	pub fn print_size(&self) {
-		let size = (self.table.len() * ENTRY_SIZE) as f32 / MEGABYTE as f32;
+		let size = (self.entries * ENTRY_SIZE) as f32 / MEGABYTE as f32;
 		println!("Transposition table size: {} MB / {} MB", size, self.size_in_mb);
 	}
 }
